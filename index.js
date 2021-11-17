5e32f3f469b316608af11c232e9e23852a90114b
ShaanWocker <ShaanWocker@gmail.com>
2021-11-18 00:44:27 +0200

    backend testing with Mongoose and routes successful

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Established"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running!");
+});
