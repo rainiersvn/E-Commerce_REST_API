8947daf82d12a311b0e053f74b13b975a01faf48
ShaanWocker <ShaanWocker@gmail.com>
2021-11-18 20:47:00 +0200

    CartSchema + ProductsSchema

--- models/User.js
+++ models/User.js
@@ -6,10 +6,10 @@ const UserSchema = new mongoose.Schema(
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         isAdmin: { 
-            type:boolean, 
+            type: Boolean, 
             default: false, 
         },
-    }, {timestamps: true}
+    }, { timestamps: true }
 );
 
-export default UserSchema;
+module.exports = mongoose.model("User", UserSchema);
